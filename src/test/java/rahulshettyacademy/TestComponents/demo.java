4a6a64b1bfff0856b3d4c750daf604cd3e290b18
Shivakumar-98 <Shivu@2024>
2025-09-01 20:42:12 +0530

    i am fianel for gitstuf

---
+++ src/test/java/rahulshettyacademy/TestComponents/demo.java
@@ -0,0 +1,13 @@
+package rahulshettyacademy.TestComponents;
+
+import org.testng.annotations.Test;
+
+public class demo 
+{
+	@Test
+	public void newdemo()
+	{
+		System.out.println("i am new demo");
+	}
+
+}
